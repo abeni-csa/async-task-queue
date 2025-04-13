23176159536e33039e20ebeb5b62484125b064c5
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-13 20:06:23 +0300

    we have seen what async is, we need to understand it in the bigger context

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,26 @@ use std::pin::Pin;
 use std::task::Poll;
 use std::time::{Duration, Instant};
 use std::{panic::catch_unwind, thread};
-fn main() {}
+fn main() {
+    let handle_one = spawn_task(sleeping(1));
+    let handle_two = spawn_task(sleeping(2));
+    let handle_three = spawn_task(sleeping(3));
+
+    println!("before the sleep");
+    std::thread::sleep(Duration::from_secs(5));
+    println!("before the block");
+
+    future::block_on(handle_one);
+    future::block_on(handle_two);
+    future::block_on(handle_three);
+}
+async fn sleeping(label: u8) {
+    println!("sleeping {}", label);
+    AsyncSleep::new(Duration::from_secs(3)).await;
+    println!("Processing sleep {}", label);
+    AsyncSleep::new(Duration::from_secs(3)).await;
+    println!("doing sleep {}", label);
+}
 fn spawn_task<F, T>(future: F) -> Task<T>
 where
     F: Future<Output = T> + Send + 'static,
