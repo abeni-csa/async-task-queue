b7085400029b9327281ce0e6e76615420ec07739
abeni-csa <abenezerhaymanot@gmail.com>
2025-04-13 18:12:54 +0300

    future can be sent between threads function prototyped

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use async_task::{Runnable, Task};
 use futures_lite::future;
 use once_cell::sync::Lazy;
 use std::pin::Pin;
-use std::{future::Future, panic::catch_unwind, thread};
+use std::{panic::catch_unwind, thread};
 
 fn main() {}
+fn spawn_task<F, T>(future: F) -> Task<T>
+where
+    F: Future<Output = T> + Send + 'static,
+    T: Send + 'static,
+{
+    todo!();
+}
